a8f6d94e52c69757d52784be471379a5e38f3a90
Dev1000 <javaimagination@gmail.com>
2024-06-27 19:43:01 -0500

    Add Chakra-UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import {Center, Text} from '@chakra-ui/react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Hello Eduardo</h1>
+    <Center>
+      <Text fontSize='4xl'>Hello Eduardo</Text>
+    </Center>
   )
 }
 
